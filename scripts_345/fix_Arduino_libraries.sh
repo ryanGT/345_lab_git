474134ad16d76850a13953e13d61725a2108183e
Ryan Krauss <ryanwkrauss@gmail.com>
2022-10-03 10:57:26 -0400

    fixing arduino install stuff

--- scripts_345/fix_Arduino_libraries.sh
+++ scripts_345/fix_Arduino_libraries.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-cp arduino-1.8.19-linuxarm.tar.xz ~/
-cd 
-tar -xvf arduino-1.8.19-linuxarm.tar.xz
-cd
-cd ~/arduino-1.8.19/
-sudo ./install.sh
-#install my libraries
-## this folder might not exist yet (until after Arduino is 
-## launched the first time)
 cd
 if [ -d "Arduino" ];
 then
