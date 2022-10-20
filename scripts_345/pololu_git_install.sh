fd2cd5a3edce264d83d332438a6b3f72e26db5d5
Ryan Krauss <ryanwkrauss@gmail.com>
2022-10-20 12:48:30 -0400

    fixed pololu

--- scripts_345/pololu_git_install.sh
+++ scripts_345/pololu_git_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Arduino/libraries
-git clone git@github.com:pololu/dual-max14870-motor-shield.git
-git clone git@github.com:pololu/qtr-sensors-arduino.git
+git clone https://github.com/pololu/dual-max14870-motor-shield.git
+git clone https://github.com/pololu/qtr-sensors-arduino.git
