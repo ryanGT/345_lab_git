56076c65fd73d482128ce1e7740f22a8d7121cc1
ryanGT <ryanwkrauss@gmail.com>
2024-10-17 17:26:24 -0400

    new fresh start bookworm stuff

---
+++ scripts_345/fresh_start_bookworm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo pip3 install --upgrade --break-system-packages krauss_misc
+./set_path.py
+./install_345_apt_packages.py
+./upgrade_drkrauss_python.sh
+./install_arduino_libraries_drkrauss.sh
